c5e1f7d1793a96f4db322ffbd768bf883937f425
KewlShubh <shubhankar.vk@gmail.com>
2023-04-14 13:51:18 +0530

    Second Commit

--- src/main/java/com/bookStore/bookStore/controller/MainController.java
+++ src/main/java/com/bookStore/bookStore/controller/MainController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class BookController {
+public class MainController {
 
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public String home_admin(){
+
+        return "home_admin";
     }
     @GetMapping("/bookRegister")
     public String bookRegister(){
-        return "bookRegister";
+
+        return "book_addition";
     }
+    @GetMapping("/getAllBooks")
+    public String getBooks(){
+
+        return "admin_display";
+
+    }
+
 }
