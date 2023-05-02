1baaa1945b2c73133d7627227a1f58f6da91ce24
UNCANNY69 <jayendraganesh21@gmail.com>
2023-05-02 18:22:00 +0530

    Final Commit

--- src/main/java/com/bookStore/bookStore/Controller/VizController.java
+++ src/main/java/com/bookStore/bookStore/Controller/VizController.java
@@ -1,20 +1,24 @@
 package com.bookStore.bookStore.Controller;
 
 import com.bookStore.bookStore.Model.Book;
+import com.bookStore.bookStore.Model.Viz1;
 import com.bookStore.bookStore.Repository.Book_Display_Repository;
+import com.bookStore.bookStore.Repository.Viz_1_Repository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Controller
 public class VizController {
-    private Book_Display_Repository bookDisplayRepository;
+    private final Book_Display_Repository bookDisplayRepository;
+    private final Viz_1_Repository viz1Repository;
 
-    public VizController(Book_Display_Repository bookDisplayRepository) {
+    public VizController(Book_Display_Repository bookDisplayRepository, Viz_1_Repository viz1Repository) {
         this.bookDisplayRepository = bookDisplayRepository;
+        this.viz1Repository = viz1Repository;
     }
 
     @GetMapping("/viz")
@@ -25,6 +29,14 @@ public class VizController {
         model.addAttribute("stock_list",book_stock);
         return "barGraph";
     }
-
+    @GetMapping("/viz2")
+    public String lineGraph(Model model){
+//        List<Viz1> list= viz1Repository.findAll();
+        List<String> user_name=viz1Repository.findAll().stream().map(Viz1::getUser_id).toList();
+        List<Integer> book_stock=viz1Repository.findAll().stream().map(Viz1::getBorrowed).toList();
+        model.addAttribute("name_list",user_name);
+        model.addAttribute("borrowed_list",book_stock);
+        return "lineGraph";
+    }
 
 }
